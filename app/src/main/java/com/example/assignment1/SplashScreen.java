673dd90836c8358257a7d2fbd4ab126da69f13f0
Jocelyn <115977531+tokziqijocelyn@users.noreply.github.com>
2024-01-17 16:55:37 +0800

    feat: fireauth signin

--- app/src/main/java/com/example/assignment1/SplashScreen.java
+++ app/src/main/java/com/example/assignment1/SplashScreen.java
@@ -8,13 +8,30 @@ import android.os.Handler;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
-
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 public class SplashScreen extends AppCompatActivity {
     private final int SPLASH_DISPLAY = 2000;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setDatabaseUrl("https://andeca2-b5af6-default-rtdb.asia-southeast1.firebasedatabase.app/")
+                .setApplicationId("1:590903525065:android:1b622cff39e82e8a631718")
+                .setApiKey("AIzaSyDmoAYjWVoJmz0JnSagYf_qMppU93I6bKc")
+                .setProjectId("andeca2-b5af6")
+                .build();
+
+        FirebaseApp.initializeApp(this, options);
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        // Initialize and assign variable
+
+
 
         ImageView image = (ImageView)findViewById(R.id.SplashScreenImage);
         ImageView imagePen = (ImageView)findViewById(R.id.pen);
@@ -35,7 +52,7 @@ public class SplashScreen extends AppCompatActivity {
             @Override
             public void run() {
                 /* Create an Intent that will start the Menu-Activity. */
-                Intent mainIntent = new Intent(SplashScreen.this,MainActivity.class);
+                Intent mainIntent = new Intent(SplashScreen.this,LoginPage.class);
                 SplashScreen.this.startActivity(mainIntent);
                 SplashScreen.this.finish();
             }
