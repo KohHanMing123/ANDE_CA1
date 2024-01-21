00d718f22ab893daca3933c0363dc2d0c37dcec2
KohHanMing123 <kohhanming123@gmail.com>
2024-01-21 14:34:35 +0800

    UI(RegisterPage): added Register page with most UI done

--- app/src/main/java/com/example/assignment1/LoginPage.java
+++ app/src/main/java/com/example/assignment1/LoginPage.java
@@ -14,6 +14,7 @@ import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.content.Intent;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -51,6 +52,16 @@ public class LoginPage extends AppCompatActivity implements View.OnClickListener
             SharedPreferences.Editor editor = sharedPreferences.edit();
             editor.putBoolean("rememberMe", isChecked);
             editor.apply();
+        });
+
+        Button registerButton = findViewById(R.id.registerButton);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginPage.this, RegisterPage.class);
+                startActivity(intent);
+            }
         });
     }
 
